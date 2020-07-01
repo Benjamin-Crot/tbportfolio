a57d82fe4d7e591540750f76549b0f8349e1416d
Tom Levy <tom@solveit.fr>
2020-07-01 16:14:35 +0200

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get "portfolio", to: "pages#portfolio"
+  get "apropos", to: "pages#apropos"
+  get "contact", to: "pages#contact"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
